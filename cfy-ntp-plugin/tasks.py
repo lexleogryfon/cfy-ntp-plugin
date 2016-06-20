945227646f1b25e6fbf2154bf2f0f5dd3bc8e5c8
lex <epam.lex@gmail.com>
2016-06-20 18:37:02 +0300

    initial commit

---
+++ cfy-ntp-plugin/tasks.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import subprocess
+from cloudify import ctx
+
+@operation
+def start(**kwargs):
+    package_manager = ctx.node.properties['package_manager']
+
+    ctx.logger.info('Installing ntp')
+    install_proc = subprocess.Popen(['sudo', package_manager, 'install', 'ntp'], stdout=subprocess.PIPE)
+
+    which_proc = subprocess.Popen(['which', 'yum'], stdout=subprocess.PIPE)
+    while True:
+        line = which_proc.stdout.readline()
+        if line != '' or r'\n':
+            #the real code does filtering here
+            
+            # we're adding a property which is set during runtime to the runtime
+            # properties of that specific node instance
+            ctx.instance.runtime_properties['ntp_path'] = line.rstrip()
+        else:
+            break
+    
+    systemctl_enable_proc = subprocess.Popen(['systemctl', 'start', 'ntp' ])
+    
+    #check if ntp is running, otherwise throw error
+    
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='cloudify-ntp-plugin',
+    version='1.0',
+    author='lex',
+    packages=['cloudify-ntp-plugin'],
+    install_requires=['cloudify-plugins-common>=3.3'],
+)
